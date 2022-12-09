2284855154297d0cb6d590c020972e8e460b9027
Aakash Mahesha <aakash280500@gmail.com>
2022-12-09 16:33:00 -0500

    test commit

--- screengrab_test.py
+++ screengrab_test.py
@@ -3,7 +3,7 @@ import cv2
 
 for i in range(0,200):
     screen = grab_screen(region=(0,0,1920,1080))  # region will vary depending on game resolution and monitor resolution
-    print(screen)
+    # print(screen)
     screen = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB) # because default will be BGR
     
     screen = cv2.resize(screen, (960,540))
